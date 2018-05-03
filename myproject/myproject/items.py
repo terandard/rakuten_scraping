f81f491ba28bfb47b75bce403ce34cd9fca78b48
terandard <38684410+terandard@users.noreply.github.com>
2018-05-03 14:32:48 +0900

    Update items.py

--- myproject/myproject/items.py
+++ myproject/myproject/items.py
@@ -17,29 +17,3 @@ class Product_review(scrapy.Item):
     title=scrapy.Field()
     contents=scrapy.Field()
     helpfullnum=scrapy.Field()
-
-
-class Review(scrapy.Item):
-    user_name=scrapy.Field()
-    age=scrapy.Field()
-    sex=scrapy.Field()
-    review_num=scrapy.Field()
-    product_name=scrapy.Field()
-    purpose=scrapy.Field()
-    user=scrapy.Field()
-    frequency=scrapy.Field()
-    evaluation=scrapy.Field()
-    date=scrapy.Field()
-    title=scrapy.Field()
-    contents=scrapy.Field()
-    helpfullnum=scrapy.Field()
-
-class Product(scrapy.Item):
-    product_id=scrapy.Field()
-    product_name=scrapy.Field()
-    author=scrapy.Field()
-    abstract=scrapy.Field()
-    user_evaluation=scrapy.Field()
-    price=scrapy.Field()
-    category=scrapy.Field()
-    review_num=scrapy.Field()
