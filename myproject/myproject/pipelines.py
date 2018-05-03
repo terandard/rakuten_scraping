eeebc2b887b896989a3f9b80cadd3eb80e9d9905
terandard <38684410+terandard@users.noreply.github.com>
2018-05-03 14:10:38 +0900

    Update pipelines.py

--- myproject/myproject/pipelines.py
+++ myproject/myproject/pipelines.py
@@ -9,24 +9,8 @@ import sqlite3
 
 
 class MyprojectPipeline(object):
-    CREATE_TABLE=('create table rakuten_review ('
-        'id integer primary key,'
-        'user_name text,'
-        'age text,'
-        'sex text,'
-        'review_num integer,'
-        'product_name text,'
-        'purpose text,'
-        'user text,'
-        'frequency text,'
-        'evaluation integer,'
-        'date text,'
-        'title text,'
-        'contents text,'
-        'helpfullnum integer'
-    ')')
     
-    CREATE_TABLE_book=('create table dvd_review ('
+    CREATE_TABLE_book=('create table review ('
         'id integer primary key,'
         'user_name text,'
         'age text,'
@@ -36,32 +20,22 @@ class MyprojectPipeline(object):
         'date text,'
         'title text,'
         'contents text,'
-        'helpfullnum integer'
+        'helpfulnum integer'
     ')')
 
-    INSERT_DATA=('insert into rakuten_review ('
-        'user_name,age,sex,review_num,'
-        'product_name,purpose,user,frequency,'
-        'evaluation,date,title,contents,helpfullnum'
-    ') values ('
-        '?,?,?,?,?,?,?,?,?,?,?,?,?'
-    ')')
-
-    INSERT_book_DATA=('insert into dvd_review ('
+    INSERT_book_DATA=('insert into review ('
         'user_name,age,sex,product_name,'
-        'evaluation,date,title,contents,helpfullnum'
+        'evaluation,date,title,contents,helpfulnum'
     ') values ('
         '?,?,?,?,?,?,?,?,?'
     ')')
 
-    DB_NAME='../hdd/DB/rakuten_scraping.db'
+    DB_NAME='rakuten_scraping.db'
     conn=None
 
     def __init__(self):
         self.conn=sqlite3.connect(self.DB_NAME)
-        if self.conn.execute("select count(*) from sqlite_master where name='rakuten_review'").fetchone()[0] == 0:
-            self.conn.execute(self.CREATE_TABLE)
-        if self.conn.execute("select count(*) from sqlite_master where name='dvd_review'").fetchone()[0] == 0:
+        if self.conn.execute("select count(*) from sqlite_master where name='review'").fetchone()[0] == 0:
             self.conn.execute(self.CREATE_TABLE_book)
 
 
@@ -70,22 +44,12 @@ class MyprojectPipeline(object):
 
 
     def process_item(self, item, spider):
-        if spider.name=='rakuten':
-            self.conn.execute(self.INSERT_DATA,(
-                item['user_name'],item['age'],item['sex'],
-                item['review_num'],item['product_name'],
-                item['purpose'],item['user'],item['frequency'],
-                item['evaluation'],item['date'],item['title'],
-                item['contents'],item['helpfullnum']
-            ))
-        """
-        """
         if spider.name=='product_rakuten':
             self.conn.execute(self.INSERT_book_DATA,(
                 item['user_name'],item['age'],item['sex'],
                 item['product_name'],
                 item['evaluation'],item['date'],item['title'],
-                item['contents'],item['helpfullnum']
+                item['contents'],item['helpfulnum']
             ))
         self.conn.commit()        
         return item
